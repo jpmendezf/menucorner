c6b21a14d3c1c476cbb1090b65a627328253b832
epiza <77017562+epiza@users.noreply.github.com>
2021-01-22 14:21:06 -0600

    Update admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Survey, Question, Option, Submission, Answer
+from .models import Menu, Lunch, Option, Submission, Answer
 
-admin.site.register(Survey)
-admin.site.register(Question)
+admin.site.register(Menu)
+admin.site.register(Lunch)
 admin.site.register(Option)
 admin.site.register(Submission)
 admin.site.register(Answer)
